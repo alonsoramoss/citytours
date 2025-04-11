f75a640dd260e9e0f241fe4448ece893d8ad10b2
Alonso Ramos <alonso24005@hotmail.com>
2025-04-11 03:26:00 -0500

    refactor: reorganiza rutas y estructura de autenticación

--- routes/web.php
+++ routes/web.php
@@ -3,25 +3,18 @@
 use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Route;
 
-
-Route::get('users', [App\Http\Controllers\UserController::class, 'index']);
-Route::get('users/create', [App\Http\Controllers\UserController::class, 'create']);
-Route::post('users/create', [App\Http\Controllers\UserController::class, 'store']);
-Route::get('users/{id}/edit', [App\Http\Controllers\UserController::class, 'edit']);
-Route::put('users/{id}/edit', [App\Http\Controllers\UserController::class, 'update']);
-Route::get('users/{id}/delete', [App\Http\Controllers\UserController::class, 'destroy']);
-
-
 Route::get('/', function () {
-    return view('welcome');
+    return redirect('/login');
+});
+
+Route::middleware(['auth'])->group(function () {
+    Route::get('users', [App\Http\Controllers\UserController::class, 'index']);
+    Route::get('users/create', [App\Http\Controllers\UserController::class, 'create']);
+    Route::post('users', [App\Http\Controllers\UserController::class, 'store']);
+    Route::get('users/{id}/edit', [App\Http\Controllers\UserController::class, 'edit']);
+    Route::put('users/{id}', [App\Http\Controllers\UserController::class, 'update']);
+    Route::delete('users/{id}', [App\Http\Controllers\UserController::class, 'destroy']);
+    Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
 });
 
 Auth::routes();
-
-Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
-
-
-
-
-
-
