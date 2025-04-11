35011a1bc616361efb19fa107fdbc05a13854930
Alonso Ramos <alonso24005@hotmail.com>
2025-04-10 21:44:16 -0500

    refactor: reorganiza y mejora estilos en login y register

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -4,47 +4,42 @@
         <title>Iniciar Sesión</title>
         <meta charset="utf-8" />
         <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no" />
-        <link
-            href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css"
-            rel="stylesheet"
-            integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN"
-            crossorigin="anonymous"
-        />
+        <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.5/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-SgOJa3DmI69IUzQ2PVdRZhwQ+dy64/BUtbMJw1MZ8t5HZApcHrRKUc4W0kG879m7" crossorigin="anonymous">
         <link rel="stylesheet" href="{{ asset('assets/estilos.css')}}">
     </head>
     <body>
-        <section class="background-radial-gradient overflow-hidden">
-            <div class="container px-4 py-5 px-md-5 text-center text-lg-start my-5">
-                <div class="row gx-lg-5 align-items-center mb-5">
-                    <div class="col-lg-6 mb-5 mb-lg-0" style="z-index: 10">
-                        <h1 class="my-5 display-5 fw-bold ls-tight" style="color: hsl(218, 81%, 95%)">
+        <section class="background-radial-gradient overflow-hidden d-flex justify-content-center align-items-center">
+            <div class="container p-2 p-md-5 text-center text-lg-start m-0 m-md-5">
+                <div class="row align-items-center">
+                    <div class="col-lg-6 mb-lg-0" style="z-index: 10">
+                        <h1 class="mb-2 display-5 fw-bold text-light">
                             CITY TOURS <br />
-                            <span style="color: hsl(218, 81%, 75%)">Empresa de turismo</span>
+                            <span class="d-none d-md-block display-6 fw-semibold" style="color: hsl(218, 81%, 75%)">Empresa de turismo</span>
                         </h1>
-                        <p class="mb-4 opacity-70" style="color: hsl(218, 81%, 85%)">
+                        <p class="d-none d-md-block" style="color: hsl(218, 81%, 85%)">
                             "¡Explora las regiones del Perú con nosotros! Inicia sesión para acceder a emocionantes recorridos guiados y descubrir los secretos mejor guardados de cada región del Perú con City Tours."
                         </p>
                     </div>
-                    <div class="col-lg-6 mb-5 mb-lg-0 position-relative">
+                    <div class="col-lg-6 px-md-4 py-md-2 p-lg-5 position-relative">
                         <div id="radius-shape-1" class="position-absolute rounded-circle shadow-5-strong"></div>
                         <div id="radius-shape-2" class="position-absolute shadow-5-strong"></div>
-                        <div class="card bg-glass mt-5">
-                            <div class="card-body ">
-                                <h4 class="text-center display-4">INICIAR SESIÓN</h4>
+                        <div class="card bg-glass">
+                            <div class="card-body">
+                                <h4 class="text-center display-6 fw-normal">INICIAR SESIÓN</h4>
                                 <form action="{{route('login')}}" method="post">
                                     @csrf
-                                    <div class="form-outline mb-4">
-                                        <label class="form-label" for="form3Example3">Correo electrónico</label>
-                                        <input type="email" name="email" id="form3Example3" class="form-control" placeholder="Ingresar correo electrónico"/>
+                                    <div class="form-outline mb-2">
+                                        <label class="form-label w-100 text-start fw-semibold" for="form3Example3">Correo electrónico</label>
+                                        <input type="email" name="email" id="form3Example3" class="form-control" placeholder="Ingresar correo electrónico" required/>
                                     </div>
-                                    <div class="form-outline mb-4">
-                                        <label class="form-label" for="form3Example4">Contraseña</label>
-                                        <input type="password" name="password" id="form3Example4" class="form-control" placeholder="Ingresar contraseña"/>
+                                    <div class="form-outline mb-2">
+                                        <label class="form-label w-100 text-start fw-semibold" for="form3Example4">Contraseña</label>
+                                        <input type="password" name="password" id="form3Example4" class="form-control" placeholder="Ingresar contraseña" required/>
                                     </div>
-                                    <div class= "text-center pt-1 mb-5">
-                                        <button class="btn btn-primary btn-block fa-lg gradient-custom-2" type="submit">Iniciar sesión</button>
+                                    <div class= "text-center my-4">
+                                        <button class="btn btn-primary" type="submit">Iniciar sesión</button>
                                     </div>
-                                    <div class="d-flex align-items-center justify-content-center pb-4">
+                                    <div class="d-flex align-items-center justify-content-center mb-2">
                                         <p class="mb-0 me-2">¿No está registrado?</p>
                                         <a href="{{route('register')}}">Crear cuenta</a>
                                     </div>
@@ -55,18 +50,7 @@
                 </div>
             </div>
         </section>
-        <script
-            src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.11.8/dist/umd/popper.min.js"
-            integrity="sha384-I7E8VVD/ismYTF4hNIPjVp/Zjvgyol6VFvRkX/vR+Vc4jQkC+hVqc2pM8ODewa9r"
-            crossorigin="anonymous"
-        ></script>
-        <script
-            src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.min.js"
-            integrity="sha384-BBtl+eGJRgqQAUMxJ7pMwbEyER4l1g+O15P+16Ep7Q9Q+zqX6gSbd85u4mG4QzX+"
-            crossorigin="anonymous"
-        ></script>
+        <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.11.8/dist/umd/popper.min.js" integrity="sha384-I7E8VVD/ismYTF4hNIPjVp/Zjvgyol6VFvRkX/vR+Vc4jQkC+hVqc2pM8ODewa9r" crossorigin="anonymous"></script>
+        <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.5/dist/js/bootstrap.min.js" integrity="sha384-VQqxDN0EQCkWoxt/0vsQvZswzTHUVOImccYmSyhJTp7kGtPed0Qcx8rK9h9YEgx+" crossorigin="anonymous"></script>
     </body>
 </html>
-
-
-
--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -4,56 +4,51 @@
         <title>Crear tu Cuenta</title>
         <meta charset="utf-8" />
         <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no" />
-        <link
-            href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css"
-            rel="stylesheet"
-            integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN"
-            crossorigin="anonymous"/>
+        <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.5/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-SgOJa3DmI69IUzQ2PVdRZhwQ+dy64/BUtbMJw1MZ8t5HZApcHrRKUc4W0kG879m7" crossorigin="anonymous">
         <link rel="stylesheet" href="{{ asset('assets/estilos.css')}}">
     </head>
     <body>
-        <section class="background-radial-gradient overflow-hidden">
-            <div class="container px-4 py-5 px-md-5 text-center text-lg-start my-5">
-                <div class="row gx-lg-5 align-items-center mb-5">
-                    <div class="col-lg-6 mb-5 mb-lg-0" style="z-index: 10">
-                        <h1 class="my-5 display-5 fw-bold ls-tight" style="color: hsl(218, 81%, 95%)">
+        <section class="background-radial-gradient overflow-hidden d-flex justify-content-center align-items-center">
+            <div class="container p-2 p-md-5 text-center text-lg-start m-0 m-md-5">
+                <div class="row align-items-center">
+                    <div class="col-lg-6 mb-lg-0" style="z-index: 10">
+                        <h1 class="mb-2 display-5 text-light fw-bold">
                             CITY TOURS <br />
-                            <span style="color: hsl(218, 81%, 75%)">Empresa de turismo</span>
+                            <span class="d-none d-md-block display-6 fw-semibold" style="color: hsl(218, 81%, 75%)">Empresa de turismo</span>
                         </h1>
-                        <p class="mb-4 opacity-70" style="color: hsl(218, 81%, 85%)">
+                        <p class="d-none d-md-block" style="color: hsl(218, 81%, 85%)">
                             "¡Únete a la aventura! Regístrate ahora para acceder a ofertas exclusivas y planificar tus próximas experiencias de viaje con City Tours."
                         </p>
                     </div>
-                    <div class="col-lg-6 mb-5 mb-lg-0 position-relative">
+                    <div class="col-lg-6 px-md-4 py-md-2 p-lg-5 position-relative">
                         <div id="radius-shape-1" class="position-absolute rounded-circle shadow-5-strong"></div>
                         <div id="radius-shape-2" class="position-absolute shadow-5-strong"></div>
-                        <div class="card bg-glass mt-5">
+                        <div class="card bg-glass">
                             <div class="card-body">
-                                <h4 class="text-center display-4">REGISTRARSE</h4>
+                                <h4 class="text-center display-6 fw-normal">REGISTRARSE</h4>
                                 <form action="{{route('register')}}" method="post">
                                     @csrf
-                                    <div class="form-outline mb-4">
-                                        <label class="form-label" for="form3Example3">Nombres y Apellidos</label>
-                                        <input type="text" name="name" id="form3Example3" class="form-control" placeholder="Ingresar nombres y apellidos"/>
+                                    <div class="form-outline mb-2">
+                                        <label class="form-label w-100 text-start fw-semibold" for="form3Example3">Nombres y Apellidos</label>
+                                        <input type="text" name="name" id="form3Example3" class="form-control" placeholder="Ingresar nombres y apellidos" required/>
                                     </div>
-                                    <div class="form-outline mb-4">
-                                        <label class="form-label" for="form3Example3">Correo electrónico</label>
-                                        <input type="email" name="email" id="form3Example3" class="form-control" placeholder="Ingresar correo electrónico"/>
+                                    <div class="form-outline mb-2">
+                                        <label class="form-label w-100 text-start fw-semibold" for="form3Example3">Correo electrónico</label>
+                                        <input type="email" name="email" id="form3Example3" class="form-control" placeholder="Ingresar correo electrónico" required/>
                                     </div>
-                                    <div class="form-outline mb-4">
-                                        <label class="form-label" for="form3Example4">Contraseña</label>
-                                        <input type="password" name="password" id="form3Example4" class="form-control" placeholder="Ingresar contraseña"/>
+                                    <div class="form-outline mb-2">
+                                        <label class="form-label w-100 text-start fw-semibold" for="form3Example4">Contraseña</label>
+                                        <input type="password" name="password" id="form3Example4" class="form-control" placeholder="Ingresar contraseña" required/>
                                     </div>
-                                    <div class="form-outline mb-4">
-                                        <label class="form-label" for="form3Example4">Confirmar contraseña</label>
-                                        <input type="password" name="password_confirmation" id="form3Example4" class="form-control" placeholder="Confirmar contraseña"/>
+                                    <div class="form-outline mb-2">
+                                        <label class="form-label w-100 text-start fw-semibold" for="form3Example4">Confirmar contraseña</label>
+                                        <input type="password" name="password_confirmation" id="form3Example4" class="form-control" placeholder="Confirmar contraseña" required/>
                                     </div>
-                                    
-                                    <div class= "text-center pt-1 mb-5">
-                                        <button class="btn btn-primary btn-block fa-lg gradient-custom-2" type="submit">Registrarse</button>
+                                    <div class= "text-center my-4">
+                                        <button class="btn btn-primary" type="submit">Registrarse</button>
                                     </div>
-                                    <div class="d-flex align-items-center justify-content-center">
-                                        <p class="mb-0 me-2">Ir a</p>
+                                    <div class="d-flex align-items-center justify-content-center mb-2">
+                                        <p class="mb-0 me-2">¿Ya tienes una cuenta?</p>
                                         <a href="{{route('login')}}">Login</a>
                                     </div>
                                 </form>
@@ -64,9 +59,6 @@
             </div>
         </section>
         <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.11.8/dist/umd/popper.min.js" integrity="sha384-I7E8VVD/ismYTF4hNIPjVp/Zjvgyol6VFvRkX/vR+Vc4jQkC+hVqc2pM8ODewa9r" crossorigin="anonymous"></script>
-        <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.min.js" integrity="sha384-BBtl+eGJRgqQAUMxJ7pMwbEyER4l1g+O15P+16Ep7Q9Q+zqX6gSbd85u4mG4QzX+" crossorigin="anonymous"></script>
+        <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.5/dist/js/bootstrap.min.js" integrity="sha384-VQqxDN0EQCkWoxt/0vsQvZswzTHUVOImccYmSyhJTp7kGtPed0Qcx8rK9h9YEgx+" crossorigin="anonymous"></script>
     </body>
 </html>
-
-
-
