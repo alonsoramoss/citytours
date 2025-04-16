d3fe7fde5ab1ed8202d9ff5eae2ccd6d2f549e1e
Alonso Ramos <alonso24005@hotmail.com>
2025-04-16 11:29:06 -0500

    refactor: agrega meta description

--- resources/views/errors/404.blade.php
+++ resources/views/errors/404.blade.php
@@ -1,19 +1,20 @@
 <!DOCTYPE html>
 <html lang="es">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Página no encontrada - 404</title>
-    <link href="https://fonts.googleapis.com/css2?family=Nunito:ital,wght@0,200..1000;1,200..1000&display=swap" rel="stylesheet">
-    <link rel="stylesheet" href="{{ asset('css/app.css') }}">
-    <link rel="stylesheet" href="{{ asset('css/404.css') }}">
-</head>
-<body class="background-radial-gradient ">
-    <div class="container-error">
-        <h1>404</h1>
-        <h2>La página que estás buscando no existe</h2>
-        <p>Verifica la dirección o vuelve a la <a href="{{ url('/') }}">página de inicio</a></p>
-        <button class="button" onclick="window.location.href='{{ url('/') }}'">Volver a la página de inicio</button>
-    </div>
-</body>
+    <head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <title>Página no encontrada - 404</title>
+        <meta name="description" content="Sistema de gestión turística que permite el registro e inicio de sesión de usuarios, visualización de paquetes turísticos y una sección para crear, editar y eliminar usuarios registrados.">
+        <link href="https://fonts.googleapis.com/css2?family=Nunito:ital,wght@0,200..1000;1,200..1000&display=swap" rel="stylesheet">
+        <link rel="stylesheet" href="{{ asset('css/app.css') }}">
+        <link rel="stylesheet" href="{{ asset('css/404.css') }}">
+    </head>
+    <body class="background-radial-gradient ">
+        <div class="container-error">
+            <h1>404</h1>
+            <h2>La página que estás buscando no existe</h2>
+            <p>Verifica la dirección o vuelve a la <a href="{{ url('/') }}">página de inicio</a></p>
+            <button class="button" onclick="window.location.href='{{ url('/') }}'">Volver a la página de inicio</button>
+        </div>
+    </body>
 </html>
