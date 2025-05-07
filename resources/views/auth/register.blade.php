471526e7d511f5804be167d4f14d55a963c6efd3
Alonso Ramos <alonso24005@hotmail.com>
2025-05-07 13:53:16 -0500

    chore: modifica la metadescripcion

--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -4,7 +4,7 @@
         <meta charset="utf-8" />
         <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no" />
         <title>Crear tu Cuenta</title>
-        <meta name="description" content="Sistema de gestión turística que permite el registro e inicio de sesión de usuarios, visualización de paquetes turísticos y una sección para crear, editar y eliminar usuarios registrados.">
+        <meta name="description" content="Sistema de gestión turística donde podrás registrarte, iniciar sesión, explorar paquetes turísticos y gestionar usuarios registrados.">
         <link href="https://fonts.googleapis.com/css2?family=Nunito:ital,wght@0,200..1000;1,200..1000&display=swap" rel="stylesheet">
         <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.5/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-SgOJa3DmI69IUzQ2PVdRZhwQ+dy64/BUtbMJw1MZ8t5HZApcHrRKUc4W0kG879m7" crossorigin="anonymous">
         <link rel="stylesheet" href="{{ asset('css/app.css') }}">
--- resources/views/errors/404.blade.php
+++ resources/views/errors/404.blade.php
@@ -4,7 +4,7 @@
         <meta charset="UTF-8">
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
         <title>Página no encontrada - 404</title>
-        <meta name="description" content="Sistema de gestión turística que permite el registro e inicio de sesión de usuarios, visualización de paquetes turísticos y una sección para crear, editar y eliminar usuarios registrados.">
+        <meta name="description" content="Sistema de gestión turística donde podrás registrarte, iniciar sesión, explorar paquetes turísticos y gestionar usuarios registrados.">
         <link href="https://fonts.googleapis.com/css2?family=Nunito:ital,wght@0,200..1000;1,200..1000&display=swap" rel="stylesheet">
         <link rel="stylesheet" href="{{ asset('css/app.css') }}">
         <link rel="stylesheet" href="{{ asset('css/404.css') }}">
